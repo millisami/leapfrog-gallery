9fb7451b0834e629cb03c5a209d6f9d3f4740bdf
Millisami <millisami@gmail.com>
2013-08-19 13:31:47 +0545

    added callback and validations in album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,7 +4,13 @@ class Album < ActiveRecord::Base
 
   has_many :photos
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :user_id, presence: true
 
+  before_save :uppercase_title
+
+  def uppercase_title
+    self.title = self.title.capitalize
+  end
 
 end
