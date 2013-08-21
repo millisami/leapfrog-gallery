0fe15e162bd93e3ced75b9a0e775241d6b975ee1
Millisami <millisami@gmail.com>
2013-08-21 12:05:23 +0545

    habtm categories added

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,15 +11,32 @@ class PhotosController < ApplicationController
     @photo = @album.photos.build(photo_params)
 
     if @photo.save
-      redirect_to album_photos_path(@album, @photo), notice: 'Photo Added'
+      redirect_to photo_path(@photo), notice: 'Photo Added'
     else
       render :new
     end
   end
 
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def udpate
+    @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo), notice: 'Photo Updated'
+    else
+      render :edit
+    end
+  end
+
 private
   def photo_params
-    params.require(:photo).permit(:caption, :description, :photo)
+    params.require(:photo).permit(:caption, :description, :photo, :category_ids)
   end
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,10 @@ class Photo < ActiveRecord::Base
 
   mount_uploader :photo, PhotoUploader
 
+  has_and_belongs_to_many :categories
+
+  def view_categories
+    self.categories.map &:name
+  end
+
 end
